1f8ae42064d6a748b16896fcc3bb717c60d7016d
a-clements <40747712+Antony-Clements@users.noreply.github.com>
2018-11-24 10:55:59 +1100

    Trying to create a custom inspector

---
+++ Assets/Editor/AccessibilityManagerEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+//[CustomEditor(typeof(AccessibilityManager))]
+public class AccessibilityManagerEditor : Editor
+{
+	// Use this for initialization
+
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    //public override void OnInspectorGUI()
+    //{
+    //    AccessibilityManager Manager = (AccessibilityManager)target;
+    //    if(GUILayout.Button("Get Buttons"))
+    //    {
+    //        Manager.GetButtons();
+    //    }
+    //}
+}
